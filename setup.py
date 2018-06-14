a2e452fa1d8efb875a711d7433e941a4459fdc9c
arrchana <arrchu@gmail.com>
2018-06-14 13:54:35 +0800

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.1',
+    description='Our team's solution for the kaggle prediction competition - Home Credit Default Risk',
+    author='CodeMonkeys',
+    license='BSD-3',
+)

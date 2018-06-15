ccf7a87d2ddd8463360a1926a3f06b1a1fb4e649
Divya Sivasankaran <div1090@gmail.com>
2018-06-15 18:13:00 +0800

    bug fix
    - description has an escape character that causes an error while
    running setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.0.1',
-    description='Our team's solution for the kaggle prediction competition - Home Credit Default Risk',
+    description='Our team\'s solution for the kaggle prediction competition - Home Credit Default Risk',
     author='CodeMonkeys',
     license='BSD-3',
 )
